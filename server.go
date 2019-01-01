3074e18b388812e382e891f5fe4ecbe95ef510be
Ken <kwill1253@gmail.com>
2019-01-01 17:39:20 -0500

    updates

--- server.go
+++ server.go
@@ -7,7 +7,8 @@ import (
 )
 
 func main() {
-	// just the start of Ulbora Cart
+	// just the start of Ulbora Cart Server
+	// This is where REST services will be defined.
 	fmt.Println("Ulbora Cart is running on port 3000!")
 	router := mux.NewRouter()
 	http.ListenAndServe(":3000", router)
